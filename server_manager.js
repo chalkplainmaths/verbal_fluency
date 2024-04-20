49f55d9e8b1d7e2a48017c41204385c29dd1adfa
Rory Phillips <rory@phillipswildsmith.com>
2024-04-20 21:00:47 +0000

    Update server_manager.js

--- server_manager.js
+++ server_manager.js
@@ -34,7 +34,7 @@ class serverManager {
 			+ this.config_json.gitlab.projectId
 			+ "/sessions/"
 			+ this.session.token
-			+ "delete";
+			+ "/delete";
 		const form = new FormData();
 		form.append("isCompleted", true);
 		this.sresponse = await this.queryServer(url, "POST", form);
