019ce97e2a495205099d9116596ea2e124133322
Rory Phillips <rory@phillipswildsmith.com>
2024-04-20 18:25:04 +0000

    Update server_manager.js

--- server_manager.js
+++ server_manager.js
@@ -30,7 +30,8 @@ class serverManager {
 	}
 	async uploadData() {
 		const form = new FormData();
-		form.append("data_001.csv", "data:text/csv;charset=utf-8,thing,otherthing,\r\n");
+		form.append("key", "data_001.csv");
+		form.append("data", "data:text/csv;charset=utf-8,thing,otherthing,\r\n");
 		const url = await this.config_json.pavlovia.URL;
 		const id = await this.config_json.gitlab.projectId;
 		const token = await this.session.token;
