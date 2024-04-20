003fd77b33c1a6666cd02a68c803c5a137e96905
Rory Phillips <rory@phillipswildsmith.com>
2024-04-20 21:39:28 +0000

    Update fluency.js

--- fluency.js
+++ fluency.js
@@ -2,7 +2,7 @@ class Fluency {
     constructor() {
         this.server_manager = new serverManager();
     }
-    start() {
-        this.server_manager.start();
+    start() { // should probably use async here
+        this.server_manager.start(); // should probably be await here, this will probably cause an error later
     }
 }
