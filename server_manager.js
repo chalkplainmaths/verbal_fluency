4d250601a7ae8c2ca4f9ae530d6fe45b18c1a3e1
Rory Phillips <rory@phillipswildsmith.com>
2024-04-20 18:35:55 +0100

    m

---
+++ server_manager.js
@@ -0,0 +1,29 @@
+class serverManager {
+	constructor() {
+		this.url_params = new URLSearchParams( window.location.search.slice(1) );
+	}
+	async getConfig() {
+		const config = await fetch("config.json");
+		this.config_json = await config.json();
+	}
+	async openSession() {
+		const form = {};
+		const url = await this.config_json.pavlovia.URL;
+		const id = await this.config_json.gitlab.projectId;
+		const full_url = url + "api/v2/experiments/" + id + "/sessions";
+		this.token = await fetch(full_url, {
+			method: "POST",
+			mode: "cors",
+			credentials: "same-origin",
+			redirect: "follow",
+			referrerPolicy: "no-referrer",
+			body: form
+		});
+	}
+	async closeSession() {
+	}
+	async uploadData() {
+	}
+	async uploadMedia() {
+	}
+}
