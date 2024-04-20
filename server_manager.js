b1a5fb538463758e6b68e0c1aa49d48995e82c25
Rory Phillips <rory@phillipswildsmith.com>
2024-04-20 18:12:25 +0000

    Update server_manager.js

--- server_manager.js
+++ server_manager.js
@@ -29,6 +29,19 @@ class serverManager {
 	async closeSession() {
 	}
 	async uploadData() {
+		const form = {"a nice key", "some lovely data"}
+		const url = await this.config_json.pavlovia.URL;
+		const id = await this.config_json.gitlab.projectId;
+		const token = await this.session.token;
+		const full_url = url + "api/v2/experiments/" + id + "/sessions" + token + "/results";
+		const response = await fetch(full_url, {
+			method: "POST",
+			mode: "cors",
+			credentials: "same-origin",
+			redirect: "follow",
+			referrerPolicy: "no-referrer",
+			body: form
+		});
 	}
 	async uploadMedia() {
 	}
