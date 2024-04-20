bd14a824a687c3f81b5506ab18314a30509fab7f
Rory Phillips <rory@phillipswildsmith.com>
2024-04-20 18:13:08 +0000

    Update server_manager.js

--- server_manager.js
+++ server_manager.js
@@ -29,7 +29,7 @@ class serverManager {
 	async closeSession() {
 	}
 	async uploadData() {
-		const form = {"a nice key", "some lovely data"};
+		const form = {"a nice key": "some lovely data"};
 		const url = await this.config_json.pavlovia.URL;
 		const id = await this.config_json.gitlab.projectId;
 		const token = await this.session.token;
