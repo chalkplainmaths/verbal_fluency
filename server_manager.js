859d4b31a12fc5c5fdc749fac00ffad7a225a146
Rory Phillips <rory@phillipswildsmith.com>
2024-04-20 17:51:12 +0000

    Update server_manager.js

--- server_manager.js
+++ server_manager.js
@@ -2,6 +2,9 @@ class serverManager {
 	constructor() {
 		this.url_params = new URLSearchParams( window.location.search.slice(1) );
 	}
+	init() {
+		new Promise( this.getConfig() ).then( this.openSession() );
+	}
 	async getConfig() {
 		const config = await fetch("config.json");
 		this.config_json = await config.json();
