3088e9210ec21fc099ca72c96c23a3a70a53d20f
Rory Phillips <rory@phillipswildsmith.com>
2024-04-25 16:54:25 +0100

    add temp media upload

--- fluency.js
+++ fluency.js
@@ -108,7 +108,7 @@ get_submission("Participant ID", true, true)
 	link.href = URL.createObjectURL(data.blob);
 })*/
 .then( (data) => {return {data: data, promise: pavlovia.start()};} )
-.then( (data) => {return pavlovia.uploadMedia(data.data.blob);} )
+.then( (data) => {return pavlovia.uploadMedia("audio."+data.data.type.split("/")[1], data.data.blob);} )
 .then( () => {return pavlovia.end();} );
 /*.then( (data) => {the_data = data; return pavlovia.start()} )
 .then( (data) => {return the_data.blob.text();})
