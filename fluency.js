8e52b95aeb50c90fa59ddf0f9677cfde9e34de69
Rory Phillips <rory@phillipswildsmith.com>
2024-05-03 22:47:50 +0000

    Update fluency.js

--- fluency.js
+++ fluency.js
@@ -106,7 +106,7 @@ let participant_id;
 let the_data;
 get_submission("Participant ID", true, true)
 .then( (value) => {participant_id = value; console.log(participant_id); return wait_button("RECORD");} )
-.then( () => {return record_audio(5000);} )
+.then( () => {return record_audio(120000);} )
 //.then( (data) => {return new Promise( (resolve) => {resolve({data: data,pavlovia: pavlovia.start()});} );} )
 .then( (data) => {return the_data = data;} )
 .then( () => {return pavlovia.start();} )
